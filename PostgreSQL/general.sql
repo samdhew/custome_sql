77f6bd7dbf0021e9f3a399c557d34e32c019ea70
Sampath Hewage <samdhew@users.noreply.github.com>
2024-01-08 17:14:43 +0530

    Update general.sql

--- PostgreSQL/general.sql
+++ PostgreSQL/general.sql
@@ -1,2 +1,2 @@
-# Check whether the PostgreSQL instance is currently in recovery mode or acting as a primary
+-- Check whether the PostgreSQL instance is currently in recovery mode or acting as a primary
 select pg_is_in_recovery();
